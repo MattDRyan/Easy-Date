b228a56eb2c29493d47997fdfcd6ac5e626fbe0d
Marcus Vinicius <marcuslops.au@gmail.comgit congif user.emailgit config --global user.name Marcus>
2022-04-19 19:42:35 +0930

    initial Version

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,8 @@
-var 
+var mapsUrl = 'https://maps.google.com';
+var netflixUrl = 'https://www.netflix.com';
+var uberEatsUrl = 'https://www.ubereats.com';
+
+fetch(mapsUrl)
+.then(function(response) {
+    
+})
--- assets/script.js
+++
@@ -1,8 +0,0 @@
-var mapsUrl = 'https://maps.google.com';
-var netflixUrl = 'https://www.netflix.com';
-var uberEatsUrl = 'https://www.ubereats.com';
-
-fetch(mapsUrl)
-.then(function(response) {
-    
-}) 

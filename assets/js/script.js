75a2a9a48bb64290b0abebaa88f0f07293e6da9a
Marcus Vinicius <marcuslops.au@gmail.comgit congif user.emailgit config --global user.name Marcus>
2022-04-21 21:34:01 +0930

    initial version of index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,22 @@
-var mapsUrl = 'https://maps.google.com';
-var netflixUrl = 'https://www.netflix.com';
-var uberEatsUrl = 'https://www.ubereats.com';
+// var mapsUrl = 'https://maps.google.com';
+// var netflixUrl = 'https://www.netflix.com';
+// var uberEatsUrl = 'https://www.ubereats.com';
 
-fetch(mapsUrl)
-.then(function(response) {
+// fetch(mapsUrl)
+// .then(function(response) {
     
-})
+// })
+// var genre1 = document.querySelector('#genre1');
+// var genre2 = document.querySelector('#genre2');
+// var genre3 = document.querySelector('#genre3');
+// var genre4 = document.querySelector('#genre4');
+// var genre5 = document.querySelector('#genre5');
+
+
+// var saveMovies = [
+//     "genre1",
+//     "genre2",
+//     "genre3",
+//     "genre4",
+//     "genre5"
+// ]

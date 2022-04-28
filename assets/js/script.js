06c27ab4b1e30f958e938a983f17222bde2adfc2
MattDRyan <39606083+MattDRyan@users.noreply.github.com>
2022-04-28 21:01:27 +0930

    Updated result URL

--- assets/js/script.js
+++ assets/js/script.js
@@ -221,7 +221,7 @@ function submitResults() {
   }
 
   // push the results to the results page via the replace method
-  window.location.replace('http://127.0.0.1:5500/Homework/Project-01/Project-1/results.html?mood='+ mood+'&cuisine='+cuisineSelection)
+  window.location.replace('/results.html?mood='+ mood+'&cuisine='+cuisineSelection)
 
 }
 
